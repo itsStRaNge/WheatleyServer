876d06ce42cacc65b0f19c7fd901c733ac07da26
Lukas Bernhard <L.J.Bernhard@web.de>
2019-08-30 13:44:59 +0200

    more clean up

--- server.h
+++ server.h
@@ -4,15 +4,13 @@
 #include <QTcpServer>
 #include <QMap>
 #include "RCSwitch.h"
-#define RED_PIN 24//24
-#define BLUE_PIN 23//23
-#define GREEN_PIN 18//18
 #define ON_RASPBERRY 1
-#define NUM_OF_SOCKETS 5
+
 #if (ON_RASPBERRY == 1)
     #include <wiringPi.h>
     #include <softPwm.h>
 #endif
+
 
 
 class server : public QTcpServer
@@ -22,10 +20,6 @@ public:
     explicit server(QObject *parent = 0);
     void setSocket(int, int);
 
-    enum{
-        SOCKET_OFF = 0,
-        SOCKET_ON = 1
-    };
 protected:
     int port;
     void incomingConnection(qintptr socketDescriptor);
